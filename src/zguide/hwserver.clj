31aeb5a8491bd7d70cfdfc60afa56af16bbef8cf
Trevor Bernard <trevor.bernard@userevents.com>
2013-07-27 00:24:30 -0300

    Fix stacktrace when context tries to close

--- src/zguide/hwserver.clj
+++ src/zguide/hwserver.clj
@@ -6,12 +6,12 @@
   (:require [zeromq.zmq :as zmq]))
 
 (defn -main []
-  (with-open [context (zmq/context 1)
-              socket (doto (zmq/socket context :rep)
-                       (zmq/bind "tcp://*:5555"))]
-    (while (not (.. Thread currentThread isInterrupted))
-      (let [reply (zmq/receive socket)]
-        (println "Received Hello")
-        ;; Do some work
-        (Thread/sleep 1000)
-        (zmq/send-str socket "World")))))
+  (let [context (zmq/context 1)]
+    (with-open [socket (doto (zmq/socket context :rep)
+                         (zmq/bind "tcp://*:5555"))]
+      (while (not (.. Thread currentThread isInterrupted))
+        (let [reply (zmq/receive socket)]
+          (println "Received Hello")
+          ;; Do some work
+          (Thread/sleep 1000)
+          (zmq/send-str socket "World"))))))
