913797202e0040b1070d9f16a51358e8d01d257d
Trevor Bernard <trevor.bernard@userevents.com>
2013-07-27 10:47:36 -0300

    Fix slow joiner problem

--- src/zguide/taskvent.clj
+++ src/zguide/taskvent.clj
@@ -13,13 +13,13 @@
 
 (defn -main []
   (let [context (zmq/zcontext 1)]
-    (println "Press Enter when the workers are ready: ")
-    (read-line)
-    (println "Sending tasks to workers…")
     (with-open [sender (doto (zmq/socket context :push)
                          (zmq/bind "tcp://*:5557"))
                 sink (doto (zmq/socket context :push)
                        (zmq/connect "tcp://127.0.0.1:5558"))]
+    (println "Press Enter when the workers are ready: ")
+    (read-line)
+    (println "Sending tasks to workers…")
       (zmq/send-str sink "0")
       (let [times (repeatedly 100 (partial work sender))]
         (printf "Total expected cost: %d msec\n" (apply + times))))))
