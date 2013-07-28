c05e1776a4ce7710da6577fd82a4fc091104fe51
Trevor Bernard <trevor.bernard@userevents.com>
2013-07-28 19:50:59 -0300

    Add mspoller and msreader

---
+++ src/zguide/mspoller.clj
@@ -0,0 +1,26 @@
+;; Reading from multiple sockets
+;; This version uses zmq_poll()
+
+(ns zguide.mspoller
+  (:require [zeromq.zmq :as zmq]))
+
+(defn -main []
+  (let [context (zmq/zcontext)
+        poller (zmq/poller context 2)]
+    (with-open [receiver (doto (zmq/socket context :pull)
+                           (zmq/connect "tcp://127.0.0.1:5557"))
+                subscriber (doto (zmq/socket context :sub)
+                             (zmq/connect "tcp://127.0.0.1:5556")
+                             (zmq/subscribe "10001"))]
+      (zmq/register poller receiver :pollin)
+      (zmq/register poller subscriber :pollin)
+      (while (not (.. Thread currentThread isInterrupted))
+        (zmq/poll)
+        (when (zmq/check-poller poller 0 :pollin)
+          (let [msg (zmq/receive receiver)]
+            ;; Process task
+            ))
+        (when (zmq/check-poller poller 1 :pollin)
+          (let [msg (zmq/receive subscriber)]
+            ;; Process weather update
+            ))))))
---
+++ src/zguide/msreader.clj
@@ -0,0 +1,17 @@
+;; Reading from multiple sockets
+;; This version uses a simple recv loop
+
+(ns zguide.msreader
+  (:require [zeromq.zmq :as zmq]))
+
+(defn -main []
+  (let [context (zmq/zcontext)]
+    (with-open [receiver (doto (zmq/socket context :pull)
+                           (zmq/connect "tcp://127.0.0.1:5557"))
+                subscriber (doto (zmq/socket context :sub)
+                             (zmq/connect "tcp://127.0.0.1:5556")
+                             (zmq/subscribe "10001"))]
+      (while (not (.. Thread currentThread isInterrupted))
+        (while (not (zmq/receive receiver zmq/dont-wait)))
+        (while (not (zmq/receive subscriber zmq/dont-wait)))
+        (Thread/sleep 1)))))
