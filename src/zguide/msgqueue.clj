613fd47da5ba5e88d01bdf8bda4cfa474ded9941
Trevor Bernard <trevor.bernard@userevents.com>
2013-07-28 21:58:35 -0300

    Add msgqueue

---
+++ src/zguide/msgqueue.clj
@@ -0,0 +1,16 @@
+;; Simple message queuing broker
+;; Same as request-reply broker but using QUEUE device
+
+(ns zguide.msgqueue
+  (:require [zeromq
+             [device :as device]
+             [zmq :as zmq]]))
+
+(defn -main []
+  (let [context (zmq/zcontext)
+        poller (zmq/poller context 2)]
+    (with-open [frontend (doto (zmq/socket context :router)
+                           (zmq/bind "tcp://*:5559"))
+                backend (doto (zmq/socket context :dealer)
+                          (zmq/bind "tcp://*:5560"))]
+      (device/proxy context frontend backend))))
