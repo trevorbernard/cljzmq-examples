9f35fdf6d426d60c39242c8874f67835423dda70
Hugh Powell <hugh@hughpowell.co.uk>
2014-09-07 22:12:13 +0100

    Remove unused poller

--- src/zguide/msgqueue.clj
+++ src/zguide/msgqueue.clj
@@ -7,8 +7,7 @@
              [zmq :as zmq]]))
 
 (defn -main []
-  (let [context (zmq/zcontext)
-        poller (zmq/poller context 2)]
+  (let [context (zmq/zcontext)]
     (with-open [frontend (doto (zmq/socket context :router)
                            (zmq/bind "tcp://*:5559"))
                 backend (doto (zmq/socket context :dealer)
